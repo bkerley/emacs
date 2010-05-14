f3acee68dfd7d4b6cb47e3531175b77ae501193f
Bryce Kerley <bkerley@brycekerley.net>
2010-05-14 14:41:56 -0400

    erlang changes

--- defunkt/erlang.el
+++ defunkt/erlang.el
@@ -1,6 +1,6 @@
-(setq load-path (cons  "/opt/local/lib/erlang/lib/tools-2.6.2/emacs"
-      load-path))
-      (setq erlang-root-dir "/opt/local/lib/erlang")
-      (setq exec-path (cons "/opt/local/lib/erlang/bin" exec-path))
-      (require 'erlang-start)
+(setq load-path (cons  "/opt/local/lib/erlang/lib/tools-2.6.4/emacs"
+     load-path))
+     (setq erlang-root-dir "/opt/local/lib/erlang")
+     (setq exec-path (cons "/opt/local/lib/erlang/bin" exec-path))
+     (require 'erlang-start)
 (defvar inferior-erlang-prompt-timeout t)
