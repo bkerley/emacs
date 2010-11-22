f9c6adcdf3f14ad3e6635960f9e837ada55b2923
Bryce Kerley <bkerley@brycekerley.net>
2010-11-22 16:27:53 -0500

    update to homebrew path

--- defunkt/erlang.el
+++ defunkt/erlang.el
@@ -1,6 +1,6 @@
-(setq load-path (cons  "/opt/local/lib/erlang/lib/tools-2.6.4/emacs"
+(setq load-path (cons  "/usr/local/Cellar/erlang/R14B/lib/erlang/lib/tools-2.6.6.1/emacs"
      load-path))
-     (setq erlang-root-dir "/opt/local/lib/erlang")
-     (setq exec-path (cons "/opt/local/lib/erlang/bin" exec-path))
+     (setq erlang-root-dir "/usr/local/Cellar/erlang/R14B/lib/erlang")
+     (setq exec-path (cons "/usr/local/Cellar/erlang/R14B/lib/erlang/bin" exec-path))
      (require 'erlang-start)
 (defvar inferior-erlang-prompt-timeout t)
